566c2fbe7c6559ba647863eeb7acea0e59e3ba51
maryzam <zamo.code@gmail.com>
2018-04-25 22:16:04 +0300

    Setup project (npm, webpack, initial scripts)

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './scripts/App';
+
+import './styles/index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
